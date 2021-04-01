c3eb1cab884fd24bb7126746d84b72f1bbbc5984
christophertsung-projects <81488563+christophertsung-projects@users.noreply.github.com>
2021-03-31 20:44:57 -0400

    Implemented Basic Classes, Started Game Loop
    Implemented Suit, Deck, and Player, started the game loop.

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Card:
+    card_suit = ""
 
+class Suit:
+    card_list = []
+    def __init__(self, suit):
+        self.card_suit = suit
+        self.card_list = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", 'Q', "K"]
+    def draw_card(self):
+        random_number = random.randint(0, 13)
+        card = self.card_list[random_number]
+        self.card_list.remove(random_number)
+        return card
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Deck:
+    suit_count = 0
+    suit_list = []
+    def __init__(self, number_of_decks):
+        self.suit_count = number_of_decks * 4
+        for x in range(number_of_decks):
+            self.suit_list.append(Suit("Earth"))
+            self.suit_list.append(Suit("Fire"))
+            self.suit_list.append(Suit("Water"))
+            self.suit_list.append(Suit("Air"))
+    def draw(self):
+        random_number = random.randint(0, len(self.suit_list) - 1)
+        card = self.suit_list[random_number].draw_card
+        if (len(self.suit_list[random_number]) == 0):
+            self.suit_list.remove(random_number)
+        return card
 
+class Player:
+    name = ""
+    hand = []
+    def __init__(self, name):
+        self.name = name
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+game_continuing = True
+while game_continuing:
+    print("Welcome to ELEMENTAL BLACKJACK!")
+    number_of_decks = int(input("Please enter how many decks you want in the game:"))
+    deck = Deck(number_of_decks)
+    players = []
+    number_of_players = int(input("Enter the number of players, now!:"))
+    for x in range(number_of_players):
+        players.append(Player(input("Enter player " + str(x + 1) + "'s name, or else!:")))
+    for x in players:
+        players[x].hand.append(deck.draw())
+        players[x].hand.append(deck.draw())
+        turn_continuing = True
+        while turn_continuing:
+            print("Would you like to hit(h) or stay(s)? Your cards are: " )
